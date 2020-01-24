a3ab619c9db7fc59bd5328f81bfd4c50502acd57
Benjamin Thom <jaminthom@gmail.com>
2020-01-24 12:03:46 -0800

    update scripts.js to display an alert if the user input is not a number

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,18 @@
 // User Interface (Front-End) Logic //
 
 $(document).ready(function(){
-  $("form#beep-boop").click(function (event) {
+  $("form#beep-boop").submit(function(event) {
   console.log("#submitButton");
     event.preventDefault();
+    var userNumber = parseInt($("input#userNumber").val());
+    var result = (userNumber);
+
+    $(".userNumber").text(userNumber);
+
+    if (isNaN(userNumber)) {
+      alert("Error, please insert a number")
+    }
+
+    $("result").show();
   });
 });
