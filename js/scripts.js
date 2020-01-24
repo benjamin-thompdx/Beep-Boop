a13f47db471c5a0527bea0beed4b7f1dbe4c50d6
Benjamin Thom <jaminthom@gmail.com>
2020-01-24 13:10:19 -0800

    update scripts.js front-end logic to only include submit button and test to make sure it works correctly with console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,13 @@
 // Business (Back-End) Logic //
 
-var surprise = function(userNumber)
 
 
 // User Interface (Front-End) Logic //
 
-$(document).ready(function(){
+$(document).ready(function() {
   $("form#beep-boop").submit(function(event){
-  // console.log("#submitButton");
+    console.log("#submitButton");
     event.preventDefault();
-    var userNumber = parseInt($("input#userNumber").val());
-    var result = surprise(userNumber);
-
-    $(".userNumber").text(userNumber);
-
-    if (isNaN(userNumber)){
-      alert("Error, please insert a number")
-    } else {
-      $("result").show();
-    }
-    //console.log(userNumber);
+    // var userNumber = parseInt($("input#userNumber").val());
   });
 });
