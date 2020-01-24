34c35641cf5978a703bea4f240f19d881dd1d7a0
Benjamin Thom <jaminthom@gmail.com>
2020-01-24 12:35:43 -0800

    experiemnt with different ways to organize logic for the number 3 within scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
 // Business (Back-End) Logic //
 
-
+var surprise = function(userNumber) {
+  for(var userNumber=1;userNumber >= 1; userNumber++) {
+    if(userNumber === 3) {
+      .show("I'm sorry, Dave. I'm afraid I can't do that.");
+    }
+  }  
+}
 
 // User Interface (Front-End) Logic //
 
 $(document).ready(function(){
   $("form#beep-boop").submit(function(event) {
-  console.log("#submitButton");
+  // console.log("#submitButton");
     event.preventDefault();
     var userNumber = parseInt($("input#userNumber").val());
-    var result = (userNumber);
+    var result = surprise(userNumber);
 
     $(".userNumber").text(userNumber);
 
     if (isNaN(userNumber)) {
       alert("Error, please insert a number")
+    } else {
+      $("result").show();
     }
-
-    $("result").show();
+    //console.log(userNumber);
   });
 });
