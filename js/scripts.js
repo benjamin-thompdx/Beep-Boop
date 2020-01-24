9a7d8088fda2e9ed4259973bdd237f779290afda
Benjamin Thom <jaminthom@gmail.com>
2020-01-24 10:53:05 -0800

    add submit button to scripts.js to gather user responses and check that it works with console.log

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,9 @@
 // User Interface (Front-End) Logic //
 
 $(document).ready(function(){
-
+  $("#submitButton").click(function (event) {
+    console.log("hello");
+    event.preventDefault();
+  });
 });
+
